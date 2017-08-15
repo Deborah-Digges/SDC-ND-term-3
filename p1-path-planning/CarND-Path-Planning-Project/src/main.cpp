ea307536e81210f49676466178749894a17c4006
ddigges <deborahgertrude_digges@intuit.com>
2017-08-15 17:50:05 +0530

    Make car stay in the same lane

--- p1-path-planning/CarND-Path-Planning-Project/src/main.cpp
+++ p1-path-planning/CarND-Path-Planning-Project/src/main.cpp
@@ -244,8 +244,20 @@ int main() {
 							double dist_inc = 0.5;
 
 							for(int i=0; i<50; ++i) {
-								next_x_vals.push_back(car_x + (dist_inc * i) * cos(deg2rad(car_yaw)));
-								next_y_vals.push_back(car_y + (dist_inc * i) * sin(deg2rad(car_yaw)));
+								// use i+1 instead of i, else the first point will be exactly where the car is at
+								// and it won't be transitioning
+								double next_s = car_s + dist_inc * (i + 1);
+
+								// we are in the middle lane
+								// the waypoints are measured from the double yellow lane in the middle of the road
+								// so we're about 1.5 lanes from where the waypoints are
+								// Each lane is 4m wide. To stay in the middle of the current lane, d should be 1.5 * 4 =  6
+								double next_d = 6;
+
+								vector<double> xy = getXY(next_s, next_d, map_waypoints_s, map_waypoints_x, map_waypoints_y);
+
+								next_x_vals.push_back(xy.at(0));
+								next_y_vals.push_back(xy.at(1));
 							}
 
 							// END
